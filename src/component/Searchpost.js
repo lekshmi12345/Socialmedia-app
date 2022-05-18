a8411fa6c6b67797cddf69cbe120ab6affbff493
lekshmi12345 <lekshmisnair213@gmail>
2022-05-18 14:33:35 +0530

    updated

--- src/component/Searchpost.js
+++ src/component/Searchpost.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 const Searchpost = () => {
   return (
     <div>
+        <Navbar/>
         <div class="container">
     <div class="row">
         <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col xl-12 col-xxl-12">
--- src/component/Viewallpost.js
+++ src/component/Viewallpost.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 const Viewallpost = () => {
 var post=[
@@ -606,6 +607,7 @@ var post=[
 
   return (
     <div>
+        <Navbar/>
 <div className="container">
     <div className="row">
         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
