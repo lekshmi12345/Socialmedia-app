0e0e4f6d94a276ccdd4942b6dfc4796572b67a40
lekshmi12345 <lekshmisnair213@gmail>
2022-05-18 13:41:32 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import Addpost from './component/Addpost';
+import Searchpost from './component/Searchpost';
 
 function App() {
   return (
     <>
+    <Searchpost/>
     <Addpost/>
     </>
   );
